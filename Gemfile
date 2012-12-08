ff5a116c9d9d6ac38e9096c14d88730bc56bcddb
John Ryan <jaryan@glgroup.com>
2012-12-08 19:50:25 +0000

    added json output

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source :rubygems
+gem 'json'
 gem 'sinatra', '1.1.0'
 gem 'thin'
--- web.rb
+++ web.rb
@@ -1,5 +1,16 @@
+require 'rubygems'
+require 'json'
 require 'sinatra'
 
+
+get '/' do
+  send_file "public/index.html"
+end
+
+get '/data' do
+  @data = {:Project => {:Name=>"Heroku-WebApp", :Author=>"John Ryan"}}
+  @data.to_json
+end
 
 get '/test' do
   "Hello, world"
